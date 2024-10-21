8c67989ef0a0243a8c9fc14ee7903512f2aa91dc
Liammgnr <Liam.mgnr@outlook.com>
2024-10-21 19:28:33 +0200

    Ajout des fichiers pour le portfolio versions v0.1

---
+++ js/script.js
@@ -0,0 +1,21 @@
+window.addEventListener('scroll', () => {
+    const elements = document.querySelectorAll('h1, h2, .contact-me-text, .contact-me-icon');
+    elements.forEach(element => {
+        const rect = element.getBoundingClientRect();
+        if (rect.top < window.innerHeight && rect.bottom >= 0) {
+            element.classList.add('scroll-animate');
+        } else {
+            element.classList.remove('scroll-animate');
+        }
+    });
+});
+
+document.addEventListener('scroll', function () {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 80) { // Ajuste cette valeur selon le moment où tu veux que la navbar apparaisse
+        navbar.classList.add('navbar-visible');
+    } else {
+        navbar.classList.remove('navbar-visible');
+    }
+});
+
